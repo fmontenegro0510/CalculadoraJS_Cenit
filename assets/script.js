4310cb8502ef12999a39b788eccfaba96d74cd16
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-05-17 12:54:44 -0300

    last changes

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
-//dark mode
+
+/*
+       _  _____    _____      _            _           _                 
+      | |/ ____|  / ____|    | |          | |         | |                
+      | | (___   | |     __ _| | ___ _   _| | __ _  __| | ___  _ __ __ _ 
+  _   | |\___ \  | |    / _` | |/ __| | | | |/ _` |/ _` |/ _ \| '__/ _` |
+ | |__| |____) | | |___| (_| | | (__| |_| | | (_| | (_| | (_) | | | (_| |
+  \____/|_____/   \_____\__,_|_|\___|\__,_|_|\__,_|\__,_|\___/|_|  \__,_|
+                                                                         
+*/ 
+//By Francisco Montenegro - 2023
+
+//Dark Mode
 flagDarkMode = false;
 const dm =()=>{
    !flagDarkMode ? document.getElementById("html").classList ="dark" : document.getElementById("html").classList ="light";
@@ -10,3 +22,12 @@ let cifra = 0;
     const clearDisplay = () => document.getElementById("display").value = 0
 
 
+//Eventos a tener en cuenta
+
+/*
+una tecla numérica (0-9)
+una tecla de operador ( + , -,×,÷)
+la tecla decimal
+la tecla de igualdad
+la tecla "clear"
+*/

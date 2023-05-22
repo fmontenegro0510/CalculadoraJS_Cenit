ae9711b8e8065827483c8df4f0452d070b45ec14
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-05-22 09:37:33 -0300

    created and test script2.js

--- assets/script2.js
+++ assets/script2.js
@@ -31,7 +31,38 @@ const resolver = () => {
     document.getElementById('display').value = y;
 }
 
-function insertNavbar(divId) {
+//contenedor
+const container = `<div id="container" class="paper container"> </div>`;
+//navbar
+const navbarHtml = `
+<nav class="border fixed split-nav">
+    <div class="nav-brand">
+    <h3><a>Calculadora Javascript</a></h3>
+    </div>
+    <div class="collapsible">
+    <input id="collapsible1" type="checkbox" name="collapsible1">
+    <label for="collapsible1">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </label>
+    <div class="collapsible-body">
+        <ul class="inline">
+        <li><a href="#" onclick="DarkMode()">Dark Mode</a></li>
+        <li><a href="https://github.com/fmontenegro0510/CalculadoraJS_Cenit" target="_blank">Github</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>
+`;
+
+//divCalculadora
+const divCalculadora = `<div id="calculadora"> </div>`;
+
+//Display
+//Botones
+
+const insertNavbar = (divId) => {
     const navbarHtml = `
     <nav class="border fixed split-nav">
         <div class="nav-brand">
@@ -55,21 +86,25 @@ function insertNavbar(divId) {
     `;
     const divElement = document.getElementById(divId);
     divElement.innerHTML = navbarHtml;
-  }
+}
 
-  
-  
-
-
-
-
-
-
+const agregarElementos = (idDiv, elto) => {
+    const divTarget = document.getElementById(idDiv);
+    divTarget.innerHTML += elto; 
+}  
 
 //Evento load - se ejecuta cuando se carga completamente la pagina  
 window.addEventListener('load', function() {
     //verifico si se ejecuto el evento load
     console.log('loaded');
+    agregarElementos('app', container);
+    agregarElementos('container', navbarHtml);
+
+
+
+
+
+
     //inserto en el div navbar la barra de navegacion
     insertNavbar('navbar');
 });
